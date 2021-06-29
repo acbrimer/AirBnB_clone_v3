a9f71de0c63ff2b8728bc8186a85cb6443d5e546
Adam <arbimer@mac.com>
2021-06-28 22:29:55 -0500

    Fix for api/v1/views/states.py-- added shebang

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python3
 """Module for api/v1 states view"""
+
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
@@ -10,12 +12,14 @@ def get_list():
     states = [s.to_dict() for s in storage.all(State).values()]
     return jsonify(states)
 
+
 @app_views.route('/states/<string:state_id>', methods=['GET'])
 def get_one(state_id):
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
+
 
 @app_views.route('/states/<string:state_id>', methods=['DELETE'])
 def delete_one(state_id):
@@ -27,6 +31,7 @@ def delete_one(state_id):
     storage.reload()
     return jsonify({}), 200
 
+
 @app_views.route('/states', methods=['POST'])
 def create():
     if not request.is_json():
@@ -37,6 +42,7 @@ def create():
     new_state = State(state_json)
     storage.new(new_state)
     return jsonify(storage.get(State, new_state.id).to_dict())
+
 
 @app_views.route('/states/<string:state_id>', methods=['PUT'])
 def update(state_id):
@@ -54,8 +60,3 @@ def update(state_id):
         del(data['updated_at'])
     state.__dict__.update(data)
     return state, 200
-
-    
-
-
-
