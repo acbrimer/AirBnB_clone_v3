086892441baa4f7f7900e931010e3a0d9354d274
Adam <arbimer@mac.com>
2021-06-28 21:44:33 -0500

    Fix for app.py-- added self param to teardown method

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """Module for api version of AirBnb app"""
 
-from api.v1.views import app_views
+
 from flask import Flask
-from models import storage
 from os import getenv
+from api.v1.views import app_views
+import models
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown():
-    storage.close()
+def teardown(self):
+    models.storage.close()
 
 
 if __name__ == "__main__":
